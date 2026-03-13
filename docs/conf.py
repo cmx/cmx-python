e2f7af4d9736e8b8ab64c7e2e4a0a1dbd3688655
Ge Yang <ge.ike.yang@gmail.com>
2026-03-13 17:39:00 -0400

    docs: remove static path and logo references
    - Set html_static_path to empty list (no static files yet)
    - Remove logo file references from Furo theme options
    - Eliminates build warnings about missing files

--- docs/conf.py
+++ docs/conf.py
@@ -40,12 +40,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+html_static_path = []  # No static files yet
 
 # Furo theme options
 html_theme_options = {
-    "light_logo": "cmx-logo-light.png",
-    "dark_logo": "cmx-logo-dark.png",
     "footer_icons": [
         {
             "name": "GitHub",
